4c60ea3391f6c9b2f7d772f2e7c239a8405e3fc9
JakupBorkowski <jborkowski2801@gmail.com>
2023-07-11 22:05:56 +0100

    test

--- activePage.js
+++ activePage.js
@@ -5,3 +5,55 @@ const navLinks = document.querySelectorAll('a').forEach(link => {
         link.classList.add('active');
     }
 })
+
+function changeLanguage()
+{
+
+    if(activePage === '/studia.html')
+    {
+        console.log('studia');
+    }
+    else if(activePage === '/index.html')
+    {
+        let data = document.getElementsByClassName("box");
+        for(let i = 0; i < data.length; i++)
+        {
+            data[i].style.display = 'none';
+        }
+        let data2 = document.getElementsByClassName("box-ang");
+        for(let i = 0; i < data2.length; i++)
+        {
+            data2[i].style.display = 'block';
+        }
+    }
+    else if(activePage === '/praca.html')
+    {
+        console.log('praca');
+    }
+}
+
+function changeLanguage2()
+{
+
+    if(activePage === '/studia.html')
+    {
+        console.log('studia');
+    }
+    else if(activePage === '/index.html')
+    {
+        let data = document.getElementsByClassName("box");
+        for(let i = 0; i < data.length; i++)
+        {
+            data[i].style.display = 'block';
+        }
+        let data2 = document.getElementsByClassName("box-ang");
+        for(let i = 0; i < data2.length; i++)
+        {
+            data2[i].style.display = 'none';
+        }
+    }
+    else if(activePage === '/praca.html')
+    {
+        console.log('praca');
+    }
+}
